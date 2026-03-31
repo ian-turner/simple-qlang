27da8f4fd0c463f69aa85c795715294bcc245358
ian-turner <i.r.turner55@gmail.com>
2026-03-31 19:19:25 -0400

    added paper downloading script

---
+++ scripts/get_papers.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+APPEL_URL="https://www.ccs.neu.edu/home/shivers/cs6983/papers/Appel,%20Andrew%20~%20Compiling%20with%20Continuations.pdf"
+
+wget -O resources/appel-continuations.pdf "$APPEL_URL"
